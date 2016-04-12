98989ad292c354ffd65dd726c34f06775523d458
gavinning <ningyubo@gmail.com>
2016-04-12 17:55:09 +0800

    优化配置向下兼容

--- uz.js
+++ uz.js
@@ -1,51 +1,41 @@
-//vi foo/index.js
-var fis = module.exports = require('fis3');
-fis.require.prefixes.unshift('uz');
-fis.cli.name = 'uz';
-fis.cli.info = require('./package.json');
+var fis, url;
 
+url = process.cwd();
+fis = module.exports = require('fis3');
+fis.cli.name = 'uz';
+fis.require.prefixes.unshift('uz');
+fis.cli.info = require('./package.json');
 // 排除某些文件
 // set 为覆盖不是叠加
-fis.set('project.ignore', ['node_modules/**', 'output/**', 'fis-conf.js', 'uzconfig.js', 'dest/**']);
+fis.set('project.ignore', ['node_modules/**', 'output/**', 'fis-conf.js', 'uzconfig.js', 'dest/**'])
 
 // ======================== other ========================
 
-fis.match(/^\/src\/(index)\.html/, {
+// For index.html
+.match(/^\/src\/(index)\.html/, {
     release: '/$1'
 })
 
-fis.match('*.{md, MD}', {
+// Filter files
+// aimee.json
+// bower.json
+// package.json
+// fis-conf.js
+// uzconfig.js
+// gruntfile.js
+// gulpfile.js
+// LICENSE
+.match(/.*\.md|(aimee|package|test|bower).json|(uzconfig|fis-conf|gruntfile|gulpfile).js|node_modules|LICENSE/i, {
     release: false
 })
 
-fis.match('LICENSE', {
+.match(/\/css\/(lib|inc)\/(.*)(less|css)/, {
     release: false
 })
-
-fis.match('uzconfig.js', {
-    release: false
-})
-
-fis.match(/\/pages\/(init)\.js/i, {
-    id: 'init',
-    isMod: true
-})
-
-fis.match('/css/{base,reset}.less', {
-    release: false
-})
-
-fis.match('{aimee, package}.json', {
-    release: false
-})
-
-fis.match('/css/{lib,inc}/*.less', {
-    release: false
-})
-
 
 // ======================== less =========================
-fis.match('*.less', {
+
+.match('*.less', {
     parser: [
         fis.plugin('less-preprocessor', {
             config: {
@@ -64,42 +54,59 @@ fis.match('*.less', {
     rExt: '.css'
 })
 
-fis.match('*.{css,less}', {
-    useHash: true,
+// 样式采用keepBreaks模式进行压缩
+.match('*.{css,less}', {
     optimizer: fis.plugin('clean-css', {
         keepBreaks : true
     })
 })
 
 // 用来控制合并时的顺序，值越小越在前面。配合 packTo 一起使用。
-fis.match('reset.less', {
-    packTo: 'home.css',
+.match('common.less', {
+    packTo: '/css/home.css',
     packOrder: -100
 })
+
+// 已在common.less中自动添加reset.less, 程序不再合并reset.less
+.match('reset.less', {
+    packTo: null,
+    packOrder: -100
+})
+
 // ======================== image ========================
 
-// 所有图片加 hash
-fis.match('image', {
-    useHash: true
-})
-
-fis.match(/\/modules\/(.*\.(?:png|jpg|jpeg|gif|svg))/i, {
+// 默认所有图片产出到 images 目录下
+.match(/\/images\/(.*\.(?:png|jpg|jpeg|gif|svg))/i, {
     release: 'images/$1'
 })
 
-fis.match(/\/widget\/(.*\.(?:png|jpg|jpeg|gif|svg))/i, {
+// modules 下的图片资源产出到 images 目录下
+.match(/\/modules\/(.*\.(?:png|jpg|jpeg|gif|svg))/i, {
     release: 'images/$1'
 })
 
-fis.match('*.png', {
+// widget 下的图片资源产出到 images 目录下
+.match(/\/widget\/(.*\.(?:png|jpg|jpeg|gif|svg))/i, {
+    release: 'images/$1'
+})
+
+// pages 下的图片资源产出到 images 目录下
+// 不推荐在虚拟页里放置多媒体资源，建议放到app目录下或放置在/images目录下
+.match(/\/pages\/(.*\.(?:png|jpg|jpeg|gif|svg))/i, {
+    release: 'images/pages/$1'
+})
+
+// 所有 png 执行压缩
+.match('*.png', {
     optimizer: fis.plugin('png-compressor', {
         type : 'pngquant'
     })
 })
 
-// ======================== jade =========================
+// ======================== js ===========================
 
-fis.match('*.jade', {
+// 所有 jade 模板执行 runtime 编译
+.match('*.jade', {
     parser: fis.plugin('jade-runtime', {
         pretty: true
     }),
@@ -108,81 +115,98 @@ fis.match('*.jade', {
     wrap: false
 })
 
-// ======================== js ===========================
-
-fis.match('*.js', {
+// js 文件默认打包到 lib.js
+.match('*.js', {
     isMod: true,
     // useHash: true,
-    optimizer: fis.plugin('uglify-js')
+    // optimizer: fis.plugin('uglify-js'),
+    packTo: '/js/pkg/lib.js'
 })
 
-fis.match('js/lib/*.js', {
+// js/lib 下的js文件不执行define包装
+.match('js/lib/*.js', {
     wrap: false
 })
 
-fis.match('/{modules,widget}/**.js', {
-    moduleId: '$1'
-})
-
-// 同名短位id pages
-fis.match(/\/pages\/([^\/]+)\/\1\.(js|coffee|es6)$/, {
+// pages 目录下的其他js
+.match(/\/pages\/(.*)\.(js|coffee|es6)$/i, {
     id: 'pages/$1',
     isMod: true
 })
 
-// pages/app/app.json
-fis.match(/\/pages\/(.*)\.(cson|json)$/i, {
-    id: 'pages/$2',
-    isMod: true,
-    important: true
-})
-
-// modules, widget
-fis.match(/\/modules\/(.*)\.(cson|json)$/i, {
-    id: '$&',
-    isMod: true
-})
-
-// modules, widget
-fis.match(/\/widget\/(.*)\.(cson|json)$/i, {
+// modules 目录下的其他js
+.match(/\/modules\/(.*)\.(js|coffee|es6)$/i, {
     id: '$1',
     isMod: true
 })
 
-// modules, widget
-fis.match(/\/modules\/(.*)\.(js|coffee|es6)$/i, {
-    id: '$1',
-    isMod: true
-})
-// modules, widget
-fis.match(/\/widget\/(.*)\.(js|coffee|es6)$/i, {
+// widget 目录下的其他js
+.match(/\/widget\/(.*)\.(js|coffee|es6)$/i, {
     id: '$1',
     isMod: true
 })
 
-// 同名短位id modules, widget
-fis.match(/\/modules\/([^\/]+)\/\1\.(js|coffee|es6)$/, {
+// 同名短位id pages
+.match(/\/pages\/([^\/]+)\/\1\.(js|coffee|es6)$/, {
+    id: 'pages/$1',
+    isMod: true
+})
+
+// 同名短位id modules
+.match(/\/modules\/([^\/]+)\/\1\.(js|coffee|es6)$/, {
     id: '$1',
     isMod: true
 })
 
-// 同名短位id modules, widget
-fis.match(/\/widget\/([^\/]+)\/\1\.(js|coffee|es6)$/, {
+// 同名短位id widget
+.match(/\/widget\/([^\/]+)\/\1\.(js|coffee|es6)$/, {
     id: '$1',
     isMod: true
 })
 
-fis.hook('commonjs', {
+.match(/\/pages\/(init)\.js/i, {
+    id: 'init',
+    isMod: true
+})
+
+// 使用commonjs标准
+.hook('commonjs', {
     baseUrl: './src',
     paths: {
         $: '/modules/zepto/zepto.js'
     }
-});
+})
 
-fis.match('/{modules,widget}/**', {
+// ======================== pack =========================
+
+// 开启同名依赖
+.match('/{modules,widget}/**', {
     useSameNameRequire: true
-});
+})
 
+.match('*.{css,less}', {
+    packTo: '/css/home.css'
+})
+
+// 打包功能
+.match(/\/(modules|widget|pages)\/(.*)\.(js)$/, {
+    packTo: '/js/pkg/mods.js'
+})
+
+// 打包模板
+.match(/\/(modules|widget|pages)\/(.*)\.(jade)$/, {
+    packTo: '/js/pkg/templates.js'
+})
+
+// 单独打包首页
+.match(/\/pages\/home\/(.*)\.(js|jade)$/, {
+    packTo: '/js/pkg/page.js'
+})
+
+// 打包模拟数据
+.match('*.json.js', {
+    packTo: '/js/pkg/json.js'
+})
 
 // ======================== post =========================
 
@@ -192,11 +216,6 @@ fis.match('/{modules,widget}/**', {
 //     postpackager: fis.plugin('loader', {})
 // });
 
-fis.media('debug').match('*.{js,css,png}', {
-    useHash: false,
-    useSprite: false,
-    optimizer: null
-})
 
 // // 启用 fis-spriter-csssprites 插件
 // fis.match('::package', {
@@ -212,27 +231,36 @@ fis.media('debug').match('*.{js,css,png}', {
 
 
 // pack
-fis
-    // 启用打包插件，必须匹配 ::package
-    .match('::package', {
-        packager: fis.plugin('map')//,
-        // spriter: fis.plugin('csssprites', {
-        //     layout: 'matrix',
-        //     margin: '15'
-        // })
-    })
-    .match('*.{css,less}', {
-        packTo: '/css/home.css'
-    })
-    .match('*.js', {
-        packTo: '/js/pkg/lib.js'
-    })
-    .match(/\/(modules|widget|pages)\/(.*)\.(js)$/, {
-        packTo: '/js/pkg/app.js'
-    })
-    .match(/\/(modules|widget|pages)\/(.*)\.(jade)$/, {
-        packTo: '/js/pkg/template.js'
-    })
-    .match('*.json.js', {
-        packTo: '/js/pkg/json.js'
-    })
+// 启用打包插件，必须匹配 ::package
+.match('::package', {
+    packager: fis.plugin('map')//,
+    // spriter: fis.plugin('csssprites', {
+    //     layout: 'matrix',
+    //     margin: '15'
+    // })
+})
+
+.match('**', {
+    deploy: [
+        fis.plugin('skip-packed'),
+        fis.plugin('local-deliver', {
+            to: require('path').join(process.env.HOME, '.uz-tmp/www')
+        })
+    ]
+})
+
+// ======================== 环境配置 =========================
+
+// For dest
+fis.media('dest').match('*.js', {
+    optimizer: fis.plugin('uglify-js'),
+})
+
+fis.media('dest').match('**', {
+    deploy: [
+        fis.plugin('skip-packed'),
+        fis.plugin('local-deliver', {
+            to: require('path').join(url, 'dest')
+        })
+    ]
+})
